df7715786a6638cd20823ab928c00c95e560d420
Sinmbf <sinmbf123@gmail.com>
2023-11-19 15:43:32 +0545

    Fixed some bugs

--- MERN/Joshua_Login-Signup/frontend/src/context/AuthContext.jsx
+++ MERN/Joshua_Login-Signup/frontend/src/context/AuthContext.jsx
@@ -18,64 +18,48 @@ export const AuthContextProvider = ({ children }) => {
   // Check the auth-status of the user
   useEffect(() => {
     const checkStatus = async () => {
-      try {
-        const data = await checkAuthStatus();
-        if (data) {
-          setUser({
-            username: data.username,
-            userType: data.userType,
-          });
-          setIsLoggedIn(true);
-        }
-      } catch (error) {
-        console.log(error);
+      const data = await checkAuthStatus();
+      if (data) {
+        setUser({
+          username: data.username,
+          userType: data.userType,
+        });
+        setIsLoggedIn(true);
       }
     };
     checkStatus();
   }, []);
   // Function to login
   const login = async (username, password, userType) => {
-    try {
-      const data = await loginUser(username, password, userType);
-      if (data) {
-        setUser({
-          username: data.username,
-          userType: data.userType,
-        });
-        setIsLoggedIn(true);
-      }
-    } catch (error) {
-      console.log(error);
+    const data = await loginUser(username, password, userType);
+    if (data) {
+      setUser({
+        username: data.username,
+        userType: data.userType,
+      });
+      setIsLoggedIn(true);
     }
   };
 
   // Function to signup
   const signup = async (username, password, userType) => {
-    try {
-      const data = await signupUser(username, password, userType);
-      if (data) {
-        setUser({
-          username: data.username,
-          userType: data.userType,
-        });
-        setIsLoggedIn(true);
-      }
-    } catch (error) {
-      console.log(error);
+    const data = await signupUser(username, password, userType);
+    if (data) {
+      setUser({
+        username: data.username,
+        userType: data.userType,
+      });
+      setIsLoggedIn(true);
     }
   };
 
   // Function to logout
   const logout = async () => {
-    try {
-      const data = await logoutUser();
-      if (data) {
-        setUser(null);
-        setIsLoggedIn(false);
-        window.location.reload();
-      }
-    } catch (error) {
-      console.log(error);
+    const data = await logoutUser();
+    if (data) {
+      setUser(null);
+      setIsLoggedIn(false);
+      window.location.reload();
     }
   };
   const value = {
--- MERN/Joshua_Login-Signup/frontend/src/helpers/apiCommunicators.js
+++ MERN/Joshua_Login-Signup/frontend/src/helpers/apiCommunicators.js
@@ -2,52 +2,48 @@ import axios from "axios";
 
 // Function to login a user
 export const loginUser = async (username, password, userType) => {
-  try {
-    const response = await axios.post("/user/login", {
-      username,
-      password,
-      userType,
-    });
-    const data = await response.data;
-    return data;
-  } catch (error) {
-    console.log(error);
+  const response = await axios.post("/user/login", {
+    username,
+    password,
+    userType,
+  });
+  if (response.status !== 200) {
+    throw new Error("Unable to login");
   }
+  const data = await response.data;
+  return data;
 };
 
 // Function to sign up a user
 export const signupUser = async (username, password, userType) => {
-  try {
-    const response = await axios.post("/user/signup", {
-      username,
-      password,
-      userType,
-    });
-    const data = await response.data;
-    return data;
-  } catch (error) {
-    console.log(error);
+  const response = await axios.post("/user/signup", {
+    username,
+    password,
+    userType,
+  });
+  if (response.status !== 201) {
+    throw new Error("Unable to create account");
   }
+  const data = await response.data;
+  return data;
 };
 
 // Function to check the auth status of a user
 export const checkAuthStatus = async () => {
-  try {
-    const response = await axios.get("/user/auth-status");
-    const data = await response.data;
-    return data;
-  } catch (error) {
-    console.log(error);
+  const response = await axios.get("/user/auth-status");
+  if (response.status !== 200) {
+    throw new Error("Unable to authenticate user");
   }
+  const data = await response.data;
+  return data;
 };
 
 // Function to logout the user
 export const logoutUser = async () => {
-  try {
-    const response = await axios.get("/user/logout");
-    const data = await response.data;
-    return data;
-  } catch (error) {
-    console.log(error);
+  const response = await axios.get("/user/logout");
+  if (response.status !== 200) {
+    throw new Error("Unable to logout user");
   }
+  const data = await response.data;
+  return data;
 };
